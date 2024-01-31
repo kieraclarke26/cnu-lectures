56dd2da52ed78036aa33d8f48b749887e68dcf4d
Charlotte Desvages <chdesvages@gmail.com>
2024-01-31 12:05:00 +0000

    Bin packing: need to loop over bins

---
+++ lectures/w03/bin_packing_charlotte.py
@@ -0,0 +1,32 @@
+def first_fit(item_list, bin_size):
+
+    # Initialise a list of bins
+    bin_list = [0]
+
+    for item in item_list:
+        # Take our ith item
+        if item <= bin_size - bin_list[0]:
+            bin_list[0] += item
+        
+        else:
+            bin_list.append(item)
+        
+    
+    return bin_list
+
+
+
+# Data we have:
+
+# size of each item
+item_list = [2, 1, 3, 2, 1, 2, 3, 1]
+
+# size of bin
+bin_size = 4
+
+# Output:
+# some list of bins with what each bin contains.
+# Solution to the question "how many bins are needed"
+# can be obtained from len(bin_list).
+
+print(first_fit(item_list, bin_size) == [4, 4, 4, 3])
---
+++ lectures/w03/conditionals_charlotte.py
@@ -0,0 +1,11 @@
+i = 25
+
+if i <= 10:
+    i += 3
+    print(f'The condition is true!')
+elif i <= 20:
+    print(f'i is <= 20, but > 10')
+else:
+    print(f'The condition is not true this time')
+
+print(f'The value of i is {i}')
