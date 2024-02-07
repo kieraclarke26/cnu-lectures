0418b0d3723a880cf7f156599dac419117cd3ede
kieraclarke26 <113912508+kieraclarke26@users.noreply.github.com>
2024-02-07 11:17:51 +0000

    week 2 and 3

---
+++ lectures/w03/conditionals.py
@@ -0,0 +1,24 @@
+i = 0
+
+while i <= 10:
+   i += 3
+   print(i)
+
+print(i)
+
+
+
+
+i = 15
+
+if i<= 10:
+   i +=3
+   print(f'The condition is true!')
+
+elif i <= 20:
+   print(f'i is <= 20 but >10')
+
+else:
+   print(f'The condition is not true this time')
+
+print(f'the value of i is {i}')
